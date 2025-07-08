e4c1893d19a0a5a3dc7f6c8d0a722180d05fa42b
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-07-08 20:43:24 +0000

    code formatting of routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,9 +7,21 @@ import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 
 const routes: Array<RouteRecordRaw> = [
-    { path: '/', name: 'Home', component: HomeView },
-    { path: '/login', name: 'Login', component: LoginView},
-    { path: '/register', name: 'Register', component: RegisterView}
+    { 
+        path: '/', 
+        name: 'Home', 
+        component: HomeView 
+    },
+    { 
+        path: '/login', 
+        name: 'Login', 
+        component: LoginView
+    },
+    { 
+        path: '/register', 
+        name: 'Register', 
+        component: RegisterView
+    }
 ]
 
 const router = createRouter({
