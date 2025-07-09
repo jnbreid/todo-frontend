9cdc63287e04189bb8f1d77044eb584d4d85f7d7
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-07-09 18:38:33 +0000

    loading of themes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicDir: false,
   plugins: [
     vue(), 
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/primevue/resources/themes/saga-blue/theme.css',
+          dest: 'themes/saga-blue'
+        },
+        {
+          src: 'node_modules/primevue/resources/themes/vela-blue/theme.css',
+          dest: 'themes/vela-blue'
+        }
+      ]
+    })
   ],
 })
